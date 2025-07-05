565719a24466c695750210899cd8043e39da2dba
Sarath TV <sarathmithran99@gmail.com>
2025-07-05 18:08:06 +0530

    created sidebar and header

---
+++ src/app/components/Header/Header.jsx
@@ -0,0 +1,45 @@
+'use client';
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+
+ const Header = () => {
+  return (
+    <div className="sticky top-0 z-50">
+        <header className="w-full h-16 px-6 flex items-center justify-between border-b bg-white dark:bg-gray-900">
+            <div className="flex items-center gap-4">
+                <SidebarTrigger className="cursor-pointer"/>
+                <h1 className="text-lg font-semibold">Dashboard</h1>
+            </div>
+
+            <div className="flex items-center gap-4">
+                <DropdownMenu>
+                <DropdownMenuTrigger>
+                    <Avatar className="cursor-pointer">
+                    <AvatarImage src="/assets/img/user.png" alt="User" />
+                    <AvatarFallback>ST</AvatarFallback>
+                    </Avatar>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>Profile</DropdownMenuItem>
+                    <DropdownMenuItem>Settings</DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>Logout</DropdownMenuItem>
+                </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
+        </header>
+    </div>
+  );
+}
+
+export default Header;
